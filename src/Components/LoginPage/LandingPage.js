219f0140f00b2d037cdd1089ff022ffcf73d2c9a
Alen George <al3ngeorge@gmail.com>
2022-05-15 19:32:07 +0530

    landing page-intial commit

---
+++ src/Components/LoginPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Content from './Content'
+import Footer from './Footer'
+import NavBar from './NavBar'
+
+function LandingPage() {
+  return (
+    <div>
+        <NavBar/>
+        <Content/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
