5610ac47ecd69cbbbef3565ca865bbe4b45d2dfd
varun-mb <kumar.varun.19.1@mountblue.tech>
2022-05-16 12:29:31 +0530

    firebase + signIn component init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import Home from "./Components/Home/Home";
+import SignIn from "./Components/SignIn+Register/SignIn";
+
 function App() {
   const user = null;
   return (
@@ -12,7 +14,7 @@ function App() {
           <LandingPage />
         ) : (
           <Switch>
-            <Route exact path="/">
+            <Route path="/">
               <Home />
             </Route>
           </Switch>
