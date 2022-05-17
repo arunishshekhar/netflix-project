a39534559d385115754c2a42d89f44e13fc36577
Alen George <al3ngeorge@gmail.com>
2022-05-17 22:46:28 +0530

    home page movies carousel design added

--- src/Components/home-page/main-content/content-list/components/array-of-content.js
+++ src/Components/home-page/main-content/content-list/components/array-of-content.js
@@ -1,39 +1,53 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import IndivisualContent from "./indivisual-content";
 import "./array-of-content.css"
 import "./carausal.css"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function ArrayOfContent(props) {
     const [pixel, transformPixel] = useState(0);
+
+    const updatePixel=(pixel)=>{
+        const track = document.getElementById(`${props.arrayOf}`)
+        track.style.transform = `translateX(-${pixel}vw)`
+
+    }
+    
+    useEffect(()=>{
+        updatePixel(pixel)
+    })
     return (
         <div className="array-of-content carausal-container" >
             <div className="carausal-container-inner">
                 <div className="track" id={props.arrayOf}>
                     {props.para.map((indiContent) => (indiContent.backdrop_path) ?
-                        <IndivisualContent para={indiContent} key={indiContent.id} /> : null)}
+                    //    <Link to={`/play/${indiContent.id}`}>  {/* </Link>  */}
+                       <IndivisualContent para={indiContent} key={indiContent.id} />
+                      
+                       : null)}
                 </div>
             </div>
             <div className="nav">
                 <button className="prev" onClick={() => {
-                    if (pixel !== 0) {
-                        const track = document.getElementById(`${props.arrayOf}`);
-                        transformPixel((prevPixel)=>prevPixel===pixel - 50)
-                        track.style.transform = `translateX(-${pixel}vw)`
+                    if (pixel >0) {
+                        transformPixel((prevPixel)=>prevPixel- 50)
+                        
                     }
+                    
                 }}>
-                    <span className="material-icons">
-                        chevron_left
-                    </span>
+                     <svg viewBox="0 0 24 24">
+              <path d="M11.56 5.56L10.5 4.5 6 9l4.5 4.5 1.06-1.06L8.12 9z"></path>
+            </svg>
                 </button>
                 <button className="next" onClick={() => {
-                    const track = document.getElementById(`${props.arrayOf}`);
-                    transformPixel(pixel + 50)
-                    track.style.transform = `translateX(-${pixel}vw)`
+                    if(pixel<500){
+                    transformPixel((prevPixel)=>prevPixel+50)
+                    console.log(pixel);
+                         }
                 }}>
-                    <span className="material-icons">
-                        chevron_right
-                    </span>
+                    <svg viewBox="0 0 24 24"><path d="M11.56 5.56L10.5 4.5 6 9l4.5 4.5 1.06-1.06L8.12 9z"></path></svg>
+                    
                 </button>
             </div>
         </div>
