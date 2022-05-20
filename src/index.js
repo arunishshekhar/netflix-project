8c5e66e6bcdf3803435a48ddd3a5af0ddd1ff1f6
varun-mb <kumar.varun.19.1@mountblue.tech>
2022-05-21 00:32:39 +0530

    dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { CookiesProvider } from 'react-cookie';
+import HomePage from './Components/home-page/home-page';
+import Fallback from './Components/FallbackPage/Fallback';
+import Payments from './Components/Payments/Payments';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-      <CookiesProvider>
-            <App />
-      </CookiesProvider>
+      <App/> 
+     //<HomePage/>
+//      <Payments/>
+     
 );
 
 // If you want to start measuring performance in your app, pass a function
